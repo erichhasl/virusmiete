bab6cb72b8b988ff11de02f3ce3f19df00bbfcae
flavis <christian@flavigny.de>
2020-03-21 10:01:38 +0100

    add empty startpage

--- miete/miete/urls.py
+++ miete/miete/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/$', startpage.urls)
+    path('', include('startpage.urls'))
 ]
